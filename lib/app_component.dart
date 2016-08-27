3960ead01aa850c355f5c90a8eb942b6d8c0922f
jeremiah <jeremiahrichter@gmail.com>
2016-08-27 17:43:03 -0500

    finish end of part 1

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -1,7 +1,24 @@
 import 'package:angular2/core.dart';
 
 @Component(
-    selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+  selector: 'my-app',
+  template: '''
+    <h1>{{title}}</h1>
+    <h2>{{hero.name}} details!</h2>
+    <div><label>ID: </label>{{hero.id}}</div>
+    <div>
+        <label>Name: </label>
+        <input [(ngModel)]="hero.name" placeholder="Name">
+    </div>''',
 )
-class AppComponent {}
+class AppComponent {
+  String title = 'Tour of Heroes';
+  Hero hero = new Hero(1, 'Windstorm');
+}
+
+class Hero {
+  final int id;
+  String name;
+
+  Hero(this.id, this.name);
+}
