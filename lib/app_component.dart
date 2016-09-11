836c38b02e93e2b0e22f1b561be965f4b960dd36
jeremiah <jeremiahrichter@gmail.com>
2016-09-11 09:12:06 -0500

    add stub dashboard component and routing

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -1,5 +1,6 @@
 import 'package:angular2/core.dart';
 import 'package:angular2/router.dart';
+import 'package:angular2_quickstart/dashboard_component.dart';
 import 'package:angular2_quickstart/heroes_component.dart';
 import 'package:angular2_quickstart/hero_service.dart';
 
@@ -9,6 +10,7 @@ import 'package:angular2_quickstart/hero_service.dart';
     <h1>{{title}}</h1>
     <nav>
       <a [routerLink]="['Heroes']">Heroes</a>
+      <a [routerLink]="['Dashboard']">Dashboard</a>
     </nav>
     <router-outlet></router-outlet>
     ''',
@@ -20,6 +22,12 @@ import 'package:angular2_quickstart/hero_service.dart';
     path: '/heroes',
     name: 'Heroes',
     component: HeroesComponent
+  ),
+  const Route(
+    path: '/dashboard',
+    name: 'Dashboard',
+    component: DashboardComponent,
+    useAsDefault: true
   )
 ])
 class AppComponent {
---
+++ lib/dashboard_component.dart
@@ -0,0 +1,9 @@
+import 'package:angular2/core.dart';
+
+@Component(
+  selector: 'my-dashboard',
+  template: '''
+  <h2>My Dashboard</h2>
+  '''
+)
+class DashboardComponent {}
