8e3dec914f5ad126cb83f782bcae93530dea7f92
jeremiah <jeremiahrichter@gmail.com>
2016-09-11 09:06:27 -0500

    configure router for heroes

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -1,16 +1,27 @@
 import 'package:angular2/core.dart';
-import 'package:angular2_quickstart/hero_component.dart';
+import 'package:angular2/router.dart';
+import 'package:angular2_quickstart/heroes_component.dart';
 import 'package:angular2_quickstart/hero_service.dart';
 
 @Component(
     selector: 'my-app',
     template: '''
     <h1>{{title}}</h1>
-    <my-heroes></my-heroes>
+    <nav>
+      <a [routerLink]="['Heroes']">Heroes</a>
+    </nav>
+    <router-outlet></router-outlet>
     ''',
-    directives: const [HeroesComponent],
-    providers: const [HeroService]
+    directives: const [ROUTER_DIRECTIVES],
+    providers: const [HeroService, ROUTER_PROVIDERS]
 )
+@RouteConfig(const [
+  const Route(
+    path: '/heroes',
+    name: 'Heroes',
+    component: HeroesComponent
+  )
+])
 class AppComponent {
   String title = 'Tour of Heroes';
 }
